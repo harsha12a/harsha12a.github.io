d4e5e20d853f6c1afb861ee1d800682575102923
harha12a <harsha.pss2005@gmail.com>
2024-10-31 09:18:42 +0530

    add assets

--- script.js
+++
@@ -1,6 +0,0 @@
-menu = document.querySelector('.menu')
-menu.addEventListener('click', () => {
-    sidebar = document.querySelector('.sidebar')
-    sidebar.classList.toggle('active');
-    menu.textContent = !sidebar.classList.contains('active')? '☰' : '✖';
-});
